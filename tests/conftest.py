de2c1815be40201b86db87a690ee6c64c54b5b16
Angelo Mosquera <anggelomos@outlook.com>
2023-10-09 11:49:27 -0500

    Added github actions cd pipeline and fixed a test issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from nothion._config import NT_AUTH
 from nothion.expense_log_model import ExpenseLog
 from nothion.personal_stats_model import TimeStats
 
-EXISTING_TEST_TASK_PAGE_ID = "af5d4552148e4c98a0b9728226350dfd"
+EXISTING_TEST_TASK_PAGE_ID = "f088993635c340cc8e98298ab93ed685"
 EXISTING_TEST_STAT_PAGE_ID = "c568738e82a24b258071e5412db89a2f"
 EXISTING_TEST_EXPENSE_LOG_PAGE_ID = "36de61f8b24c49e286bb5b0aca9740ab"
 
